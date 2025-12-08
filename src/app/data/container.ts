716fab9eb3ef1e90ee55b1d43b5aa5ef7566007a
MASSIL <massilaitchallal2002@gmail.com>
2025-12-08 15:24:18 +0100

    Creation of the first data interfaces: 1-Flavour with name and stock, 2-Extrawith name, stock, price, 3-Container with type <cone> or <cup> and the price

---
+++ src/app/data/container.ts
@@ -0,0 +1,4 @@
+export interface Container {
+    type: 'cone' | 'cup';
+    price: number; // cone = 1euro , cup = 0 euro
+}
---
+++ src/app/data/flavor.ts
@@ -0,0 +1,5 @@
+export interface Flavor {
+    name: string;
+    stock: number; //quantite en ml
+
+}
