c0ea5ac16d3c3363f52bc790f0e0a8edf7a8fd9e
MASSIL <massilaitchallal2002@gmail.com>
2025-12-08 16:01:19 +0100

    Adding the Injection in main.ts : the provider provide an instance of IceCreamRepositoryDummyImpl when we ask for an instance of IceCreamRepository

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { IceCreamRepository } from './app/repository/ice-cream-repository';
+import { IceCreamRepositoryDummyImpl } from './app/repository/ice-cream-repository-dummy-impl';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { 
+      provide : IceCreamRepository , useClass : IceCreamRepositoryDummyImpl},
+
+      {provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
